565189802298becdfe448af591005b764aef7b43
utkarshg1 <gaikwadujg@gmail.com>
2024-07-29 04:13:26 +0530

    updated index.js

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ const app = express();
 const port = 3000;
 db.connect();
 
-let quiz = [
-  { country: "France", capital: "Paris" },
-  { country: "United Kingdom", capital: "London" },
-  { country: "United States of America", capital: "New York" },
-];
+let quiz = [];
 
 db.query("SELECT * FROM capitals", (err, res) => {
   if (err) {
